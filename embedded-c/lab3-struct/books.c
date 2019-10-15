69feff331119279d9434c799e0b9f0a2af337bd5
Dmitry Sologub <dd.sologub@gmail.com>
2019-10-15 17:43:32 +0700

    Dynamic implementation

--- embedded-c/lab3-struct/books.c
+++ embedded-c/lab3-struct/books.c
@@ -16,41 +16,47 @@ void read_book(Book *cur_book)
 	scanf("%d", &cur_book->price);
 }
 
-void print_book(Book cur_book)
+void print_book(Book *cur_book)
 {
-	printf("Title:\t%s\n", cur_book.title);
-	printf("Published:\t%d\n", cur_book.publish_date);
-	printf("Pages:\t%d\n", cur_book.pages_num);
-	printf("Price:\t%d\n", cur_book.price);
+	printf("Title:\t%s\n", cur_book->title);
+	printf("Published:\t%d\n", cur_book->publish_date);
+	printf("Pages:\t%d\n", cur_book->pages_num);
+	printf("Price:\t%d\n", cur_book->price);
 	printf("\n");
 }
 
 int cmp_book(const void *p1, const void *p2)
 {
-	Book *b1 = (Book*)p1;
-	Book *b2 = (Book*)p2;
-
+	Book *b1 = *(Book**)p1;
+	Book *b2 = *(Book**)p2;
+	fprintf(stderr, "%d - %d\n", (b1->price), (b2->price));
 	return (b1->price - b2->price);
 }
 
 int main(void) {
-	const int kMaxBookNum = 100;
-	Book books[kMaxBookNum];
 	
 	int n = 0;
 	scanf("%d", &n);
+	
+	Book** books = (Book**)malloc(sizeof(Book*) * n);
 
 	for(int i = 0; i < n; i++)
 	{
-		read_book(&books[i]);
+		books[i] = (Book*)malloc(sizeof(Book));
+		read_book(books[i]);
 	}
 
-	qsort(books, n, sizeof(Book), cmp_book);
+	qsort(&books[0], n, sizeof(Book*), cmp_book);
 
 	for(int i = 0; i < n; i++)
 	{
 		print_book(books[i]);
 	}
+
+	for (int i = n-1; i >= 0; i--) {
+		free(books[i]);
+	}
+	free(books);
 
 	return 0;
 }
