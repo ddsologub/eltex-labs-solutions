aca5683602ae72925e97278bd7860f22e86379c6
Dmitry Sologub <dd.sologub@gmail.com>
2019-11-12 15:03:18 +0700

    Agrumgents via cmd

--- embedded-c/lab4-files/changechar.c
+++ embedded-c/lab4-files/changechar.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-	FILE *fin, *fout;
-	
-	if ((fin = fopen("imdb250.txt", "r")) == NULL) {
-		fprintf(stderr, "Cannot open input file\n");
-		exit(1);	
+int main(int argc, char *argv[])
+{
+	if (argc < 3) {
+		fprintf(stderr, "Too few arguments. Use: changechar <filename> <char>\n");
+		exit(1);
 	}
 
-	if ((fout = fopen("imdb250_output.txt", "w")) == NULL) {
+	char inp_ch = ' ';
+	char foutname[255];
+	strcpy(foutname, argv[1]);
+	strcat(foutname, ".out");
+
+	FILE *fin, *fout;
+	
+	if ((fin = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "Cannot open input file %s\n", argv[1]);
+		exit(1);
+	}
+
+	if ((fout = fopen(foutname, "w")) == NULL) {
 		fprintf(stderr, "Cannot open output file\n");
-		exit(1);	
+		exit(1);
 	}
 
 	char ch = 0;
 	while(ch != EOF) {
 		//fread(&ch, sizeof(char), 1, fin);
 		ch = getc(fin);
-		if (ch != ' ') {
+		if (ch != inp_ch) {
 			//fwrite(&ch, sizeof(char), 1, fout);
 			fputc(ch, fout);
 		}
